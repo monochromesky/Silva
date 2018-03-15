3776c3dea3fb48e6d44a8e3990ae9c1c61899f69
Wafieqa AZ <maguu.samurai@gmail.com>
2018-03-15 14:39:37 +0800

    initial commit

---
+++ SilvasPotion/Assets/Scripts/UI/ButtonManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void PlayGameBtn(string gameLevel) //Pass in the name of the screen/level we're going to load in this function
+    {
+        SceneManager.LoadScene(gameLevel);    //Let us do a button press
+    }
+    public void ExitGame()
+    {
+        Debug.Log("Quit!"); //Debugging purposes
+        Application.Quit();
+    }
+}
